8d1427388a8caeca1ef514debc9f9a1b91db2296
Logan Smith <smithla1@g.cofc.edu>
2016-10-31 10:48:10 -0400

    Add files via upload

---
+++ registrationID.java
@@ -0,0 +1,15 @@
+public class registrationID {
+    private byte[] secureRegistrationID;
+    
+    private String createRegistrationID( String[] PID) {
+        return null;
+    }
+    
+    private byte[] createSecureRegistrationID( String registrationID ) {
+        return null;
+    }
+    
+    private byte[] getSecureRegistrationID() {
+        return secureRegistrationID;
+    }
+}
