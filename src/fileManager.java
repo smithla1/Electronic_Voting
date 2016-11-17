adef19de7305477849ad38a662ea14fc916e687e
Logan Smith <smithla1@g.cofc.edu>
2016-11-16 20:36:27 -0500

    Completed inRegistrationDB() method

--- src/fileManager.java
+++ src/fileManager.java
@@ -2,10 +2,13 @@ import java.io.FileWriter;
 import java.io.FileInputStream;
 import java.lang.StringBuffer;
 import java.io.IOException;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 
 public class fileManager {
     private FileWriter registration_out_file = null;
-    private String registration_in_file = "registrationLogFile.csv";
+    private FileReader registration_in_file = null;
     private String voter_out_file = "voterLogFile.csv";
     private String voter_in_file = "voterLogFile.csv";
     private String results_out_file = "resultsLogFile.csv";
@@ -23,13 +26,12 @@ public class fileManager {
                 information.append(PID[i]);
                 information.append(",");
             }
-            information.append(encryptedRegistrationID);
+            information.append(encryptedRegistrationID.toString());
             information.append("\n");
 
             registration_out_file.write(information.toString());
             registration_out_file.close();
-
-            System.out.println(information);
+            registration_out_file = null;
 
         } catch (IOException e) {
             System.err.println("IOException: " + e.getMessage());
@@ -37,7 +39,32 @@ public class fileManager {
     }
     
     private boolean inRegistrationDB( byte[] encryptedRegistrationID ) {
-        return false;
+        try {
+            registration_in_file = new FileReader("regLog.csv");
+            BufferedReader br = new BufferedReader(registration_in_file);
+
+            String line = br.readLine();
+            while( line != null ) {
+                if ( encryptedRegistrationID.toString().equals(line.split(",")[3])) {
+                    return true;
+                }
+                line = br.readLine();
+            }
+
+            return false;
+
+        } catch (FileNotFoundException fnfe) {
+            // The file was not found
+            // This could mean that no one has been registered to vote,
+            //  as the file would exist if someone had.
+            // In this case, the person we are checking for hasn't registered
+            //  so we should return false.
+            return false;
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return false;
+        }
     }
     
     private boolean inVoterDB( byte[] encryptedRegistrationID ) {
@@ -57,9 +84,19 @@ public class fileManager {
     }
 
     public static void main(String[] args) {
+        //Create a fileManager class
         fileManager myManager = new fileManager();
-        String[] myPID = {"Logan", "Allen", "Smith"};
-        myManager.addRegisteredVoter(new byte[0], myPID);
 
+        //Create some dummy data
+        String[] myPID = {"Logan", "Werbenjagermanjensen", "Smith"};
+        byte[] testID = new byte[8];
+
+        //Register this dummy
+        myManager.addRegisteredVoter(testID, myPID);
+
+        System.out.print("Logan is registered to vote: ");
+
+        //Test finding an entry
+        System.out.println(myManager.inRegistrationDB(testID));
     }
 }
