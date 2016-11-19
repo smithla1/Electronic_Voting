e704645c86dfb8dfa8d398e90e579ee6de7d116a
Logan Smith <smithla1@g.cofc.edu>
2016-11-18 23:06:52 -0500

    Removed encryption from SystemLogic.java

--- src/SystemLogic.java
+++ src/SystemLogic.java
@@ -8,17 +8,11 @@ public class SystemLogic {
     private String[] candidates;
     private fileManager myManager;
     private registrationID currentUserID;
-    private String saltString;
-    private byte[] salt;
-    private PasswordEncryptionService encryptionService;
     
     public SystemLogic() {
         //initialize and or connect to database
         isAdministrator = false; //set this to false by default, according to our admin log in method
         myManager = new fileManager();
-        saltString = "THISISTHEMASTERPASSWORD";
-        salt = saltString.getBytes();
-        encryptionService = new PasswordEncryptionService();
     }
    
     protected boolean registerOrVote(boolean decision){
@@ -41,13 +35,13 @@ public class SystemLogic {
     	}
     }
     
-    protected void registerUser(String[] PID, byte[] secureRegistrationID) throws Exception{ //The following three methods are different from our deliverable
-        myManager.addRegisteredVoter(secureRegistrationID, PID);
+    protected void registerUser(String[] PID, String registrationID) throws Exception{ //The following three methods are different from our deliverable
+        myManager.addRegisteredVoter(registrationID, PID);
     }
     
-    protected boolean userIsRegistered(byte[] secureRegistrationID) throws Exception {
+    protected boolean userIsRegistered(String registrationID) throws Exception {
     	try{
-    		if(myManager.isRegistered(secureRegistrationID)){
+    		if(myManager.isRegistered(registrationID)){
     			return true;
     		}
     		else{
@@ -61,24 +55,10 @@ public class SystemLogic {
     		
         }
     }
-
-    protected boolean userIsRegistered(String secureRegistrationID) throws Exception{
-        try {
-            if(myManager.isRegistered(encryptionService.getEncryptedPassword(secureRegistrationID, salt))) {
-                return true;
-            } else {
-                return false;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("There was an error checking if the user was registered");
-            throw e;
-        }
-    }
     
     protected boolean userHasVoted(String registrationID) {
         try {
-			return myManager.hasVoted(currentUserID.getSecureRegistrationID());
+			return myManager.hasVoted(currentUserID.getRegistrationID());
 		} catch (Exception e){
 			System.out.println("There was an error determining if this user has voted");
 			//return true so user cannot vote if an error occurs at this stage.
@@ -92,7 +72,7 @@ public class SystemLogic {
     }
     
     protected void castVote(String[] selection) {
-        myManager.castVote(selection, currentUserID.getSecureRegistrationID());
+        myManager.castVote(selection, currentUserID.getRegistrationID());
     }
     
     protected void printBallot(String[] selection) { //Different from our third deliverable
